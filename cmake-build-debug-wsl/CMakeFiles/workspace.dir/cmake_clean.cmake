4abefeffd005168b0c0835ff56651b1f49b97785
huchenfan <15779163358@163.com>
2025-07-20 10:34:28 +0800

    初始化

---
+++ cmake-build-debug-wsl/CMakeFiles/workspace.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/workspace.dir/main.cpp.o"
+  "CMakeFiles/workspace.dir/main.cpp.o.d"
+  "workspace"
+  "workspace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/workspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
